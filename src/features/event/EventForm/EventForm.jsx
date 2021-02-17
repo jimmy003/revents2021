4dc1335330f00d057f7cdaa984d02431bfa7572e
jimmy003 <jimmy003.deguzman@gmail.com>
2021-02-17 22:55:40 +0800

    End of Section 5 CRUD

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -10,9 +10,23 @@ export class EventForm extends Component {
     hostedBy: '',
   };
 
+  // !life cycle method
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
+      this.setState({
+        ...this.props.selectedEvent,
+      });
+    }
+  }
+
+  // * helper methods / functions
   handleFormSubmit = (evt) => {
     evt.preventDefault();
-    this.props.createEvent(this.state);
+    if (this.state.id) {
+      this.props.updateEvent(this.state);
+    } else {
+      this.props.createEvent(this.state);
+    }
   };
 
   handleInputChange = ({ target: { name, value } }) => {
